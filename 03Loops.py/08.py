a09d91b72ee065c5059631682ad0ae86d00a427e
Piyushrathoree <meonpath008@gmail.com>
2025-02-13 02:34:25 +0530

    add prime number checker script

---
+++ 03Loops.py/08.py
@@ -0,0 +1,11 @@
+#  8. Prime Number Checker
+#  Problem: Check if a number is prime.
+number = int (input ("enter your number:"))
+is_prime = True
+if number > 1 :
+    for i in range(2, number):
+        if (number %i)==0:
+            is_prime = False
+
+
+print(is_prime)
