7d91ee61dd6046fb4a55ac65c8aa1edc59aedccb
Piyushrathoree <meonpath008@gmail.com>
2025-07-09 01:34:08 +0530

    refactor: improve code formatting and readability in function.py, function2.py, and regexHex.py

--- questions/function2.py
+++ questions/function2.py
@@ -1,18 +1,16 @@
-n=int(input("Enter the number:"))
+n = int(input("Enter the number:"))
+
+
 def printConsecutiveNum(n):
-    output= []
-    for i in range(0,n+1):
-        if(i<n+1):
+    output = []
+    for i in range(0, n+1):
+        if (i < n+1):
             output.append(i)
-  
-  
-  
-  
-  
-  
+
     str_list = [str(num) for num in output]
     combined_str = "".join(str_list)
     result = int(combined_str)
     return result
-    
-print(printConsecutiveNum(n))  
+
+
+print(printConsecutiveNum(n))f
--- questions/regexHex.py
+++ questions/regexHex.py
@@ -3,20 +3,18 @@ def check(line):
     start = line.index("#")
     end = line.index(";")
     print(line[start:end])
-    
-    
+
+
 n = int(input())
 code = []
 for i in range(n):
-    line=input().split()
-    if len(line)>0:
+    line = input().split()
+    if len(line) > 0:
         for k in range(len(line)):
-            
+
             if "#" in line[k] and ";" in line[k]:
                 check(line[k])
                 code.append(line[k])
-   
 
 
-
-#preety complex logic for handling multiple comments and here the time complexity is broken 
+# preety complex logic for handling multiple comments and here the time complexity is broken
