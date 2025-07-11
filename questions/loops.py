f38d125947283c17873e7f6e39afc03e39787993
Piyushrathoree <meonpath008@gmail.com>
2025-07-11 11:29:36 +0530

    fix: update nums list and clean up commented code for clarity

--- questions/loops.py
+++ questions/loops.py
@@ -1,12 +1,11 @@
 import math
 n = input("Enter a number: ")
 
-for i in range (0,n):
-    if(i<n):
-        ans = int(math.pow(i,2))
-        print(ans)
+# for i in range (0,n):
+#     if(i<n):
+#         ans = int(math.pow(i,2))
+#         print(ans)
 
-nums = [1,2,3,4]
-for i in nums:
+nums = [1, 5, 3, 4]
+for i in range(0, len(nums)):
     print(nums[i])
-    a
